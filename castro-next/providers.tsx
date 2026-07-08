4596633f72de16af218b519683dccb16fa5cb100
Castro336488 <loveallx3365@gmail.com>
2026-07-08 10:58:06 +0000

    fix: remove wallet adapter from providers

--- castro-next/providers.tsx
+++ castro-next/providers.tsx
@@ -1,14 +1,6 @@
 "use client";
-import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { ReactNode } from "react";
 
-const wallets = [new PetraWallet()];
-
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <AptosWalletAdapterProvider plugins={wallets} autoConnect={false}>
-      {children}
-    </AptosWalletAdapterProvider>
-  );
+  return <>{children}</>;
 }
