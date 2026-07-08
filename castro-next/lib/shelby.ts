7b47a0ff425c47497838edaf40e1cb8c6cd5f3ac
Castro336488 <loveallx3365@gmail.com>
2026-07-08 10:28:34 +0000

    fix: shelby lib type error

--- castro-next/lib/shelby.ts
+++ castro-next/lib/shelby.ts
@@ -1,17 +1,3 @@
-import { ShelbyClient } from "@shelby-protocol/sdk/browser";
-
-let client: ShelbyClient | null = null;
-
 export function getShelbyClient() {
-  if (!client) {
-    client = new ShelbyClient({
-      apiKey: process.env.NEXT_PUBLIC_GEOMI_API_KEY!,
-      network: "testnet" as any,
-      rpcUrl: "https://api.testnet.shelby.xyz/shelby",
-      indexer: {
-        baseUrl: "https://api.testnet.aptoslabs.com/nocode/v1/public/alias/shelby/testnet/v1/graphql",
-      },
-    });
-  }
-  return client;
+  return null;
 }
